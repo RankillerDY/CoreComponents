69f32fc450d22e41dc2d0f18e386ea573e20862a
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2024-05-08 20:04:36 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ backgroundColor: "plum", flex: 1 }}>
+      <View
+        style={{ backgroundColor: "lightblue", width: 200, height: 200 }}
+      ></View>
+      <View
+        style={{ backgroundColor: "lightgreen", width: 200, height: 200 }}
+      ></View>
+    </View>
+  );
+}
